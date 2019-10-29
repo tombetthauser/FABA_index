cdbcae535f3d86a50838db1f42b0e79cd032e80f
tombetthauser <tombetthauser@gmail.com>
2019-10-29 15:38:48 -0700

    Initial commit with initialize added to Faba class

---
+++ faba_model.rb
@@ -0,0 +1,12 @@
+class Faba
+
+  attr_reader :a_con, :a_ben, :exp, :examples
+
+  def initialize(a_con, a_ben, exp)
+    @a_con = a_con
+    @a_ben = a_ben
+    @exp = exp
+    @examples = []
+  end
+
+end
